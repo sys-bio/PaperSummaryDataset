f9ee39b6a1defcf388e50ecaf9746b7f302f756a
adelhpour <ad.heydarabadi@gmail.com>
2024-09-27 12:29:30 -0700

    The initial design of the project is implemented.

---
+++ test/test1.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from data_record_creator import data_record_creator
+
+path_to_models = "models"
+data_record_creator = data_record_creator.DataRecordCreator()
+data_record_creator.create(path_to_models)
